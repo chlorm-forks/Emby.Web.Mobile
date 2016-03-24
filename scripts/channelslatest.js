7f529d591e6c31c6218bbff6a549166b1fa9989e
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-03-24 14:12:14 -0400

    update nav tabs

--- scripts/channelslatest.js
+++ scripts/channelslatest.js
@@ -1,3 +1,3 @@
 ﻿define([],function(){function reloadItems(page){Dashboard.showLoadingMsg();Sections.loadLatestChannelItems(page.querySelector('.latestItems'),Dashboard.getCurrentUserId()).then(function(){Dashboard.hideLoadingMsg();},function(){Dashboard.hideLoadingMsg();});}
 function loadTab(page,index){switch(index){case 0:reloadItems(page);break;default:break;}}
-pageIdOn('pageinit',"channelsPage",function(){var page=this;var pages=page.querySelector('neon-animated-pages');pages.addEventListener('tabchange',function(e){loadTab(page,parseInt(e.target.selected));});});});
+pageIdOn('pageinit',"channelsPage",function(){var page=this;var pageTabsContainer=page.querySelector('.pageTabsContainer');pageTabsContainer.addEventListener('tabchange',function(e){loadTab(page,parseInt(e.detail.selectedTabIndex));});});});
--- scripts/photos.js
+++ scripts/photos.js
@@ -10,10 +10,10 @@ query.ParentId=getParameterByName('parentId')||LibraryMenu.getTopParentId();}
 function startSlideshow(page,itemQuery,startItemId){var userId=Dashboard.getCurrentUserId();var localQuery=$.extend({},itemQuery);localQuery.StartIndex=0;localQuery.Limit=null;localQuery.MediaTypes="Photo";localQuery.Recursive=true;localQuery.Filters="IsNotFolder";ApiClient.getItems(userId,localQuery).then(function(result){showSlideshow(page,result.Items,startItemId);});}
 function showSlideshow(page,items,startItemId){var index=items.map(function(i){return i.Id;}).indexOf(startItemId);if(index==-1){index=0;}
 require(['slideshow'],function(slideshow){var newSlideShow=new slideshow({showTitle:false,cover:false,items:items,startIndex:index,interval:7000,interactive:true});newSlideShow.show();});}
-function onListItemClick(e){var page=$(this).parents('.page')[0];var info=LibraryBrowser.getListItemInfo(this);if(info.mediaType=='Photo'){var tab=page.querySelector('neon-animated-pages').selected;var query=getQuery(tab);Photos.startSlideshow(page,query,info.id);return false;}}
+function onListItemClick(e){var page=$(this).parents('.page')[0];var info=LibraryBrowser.getListItemInfo(this);if(info.mediaType=='Photo'){var query=getQuery(LibraryBrowser.selectedTabIndex(page.querySelector('.pageTabsContainer')));Photos.startSlideshow(page,query,info.id);return false;}}
 function loadTab(page,index){switch(index){case 0:{reloadItems(page.querySelector('.albumTabContent'),0);}
 break;case 1:{reloadItems(page.querySelector('.photoTabContent'),1);}
 break;case 2:{reloadItems(page.querySelector('.videoTabContent'),2);}
 break;default:break;}}
 pageIdOn('pageinit',"photosPage",function(){var page=this;var tabs=page.querySelector('paper-tabs');var baseUrl='photos.html';var topParentId=LibraryMenu.getTopParentId();if(topParentId){baseUrl+='?topParentId='+topParentId;}
-LibraryBrowser.configurePaperLibraryTabs(page,tabs,page.querySelector('neon-animated-pages'),baseUrl);page.querySelector('neon-animated-pages').addEventListener('tabchange',function(e){loadTab(page,parseInt(e.target.selected));});$(page).on('click','.mediaItem',onListItemClick);});window.Photos={startSlideshow:startSlideshow};});
+LibraryBrowser.configurePaperLibraryTabs(page,tabs,page.querySelector('.pageTabsContainer'),baseUrl);page.querySelector('.pageTabsContainer').addEventListener('tabchange',function(e){loadTab(page,parseInt(e.detail.selectedTabIndex));});$(page).on('click','.mediaItem',onListItemClick);});window.Photos={startSlideshow:startSlideshow};});
