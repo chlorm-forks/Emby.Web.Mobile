c4ae63679e4035cdd68ebe2e879f93c5e40bfe02
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-11 13:25:02 -0400

    update components

--- scripts/livetvcomponents.js
+++ scripts/livetvcomponents.js
@@ -1,9 +1,10 @@
 ﻿define(['datetime'],function(datetime){function enableScrollX(){return browserInfo.mobile&&AppInfo.enableAppLayouts;}
+function getSquareShape(){return enableScrollX()?'overflowSquare':'square';}
 function getTimersHtml(timers){var items=timers.map(function(t){t.Type='Timer';return t;});var groups=[];var currentGroupName='';var currentGroup=[];var i,length;for(i=0,length=items.length;i<length;i++){var item=items[i];var dateText='';if(item.StartDate){try{var premiereDate=datetime.parseISO8601Date(item.StartDate,true);dateText=LibraryBrowser.getFutureDateText(premiereDate,true);}catch(err){}}
 if(dateText!=currentGroupName){if(currentGroup.length){groups.push({name:currentGroupName,items:currentGroup});}
 currentGroupName=dateText;currentGroup=[item];}else{currentGroup.push(item);}}
 var html='';for(i=0,length=groups.length;i<length;i++){var group=groups[i];html+='<div class="homePageSection">';html+='<h1 class="listHeader">'+group.name+'</h1>';if(enableScrollX()){html+='<div class="itemsContainer hiddenScrollX">';}else{html+='<div class="itemsContainer">';}
-html+=LibraryBrowser.getPosterViewHtml({items:group.items,shape:"square",showTitle:true,showAirTime:true,showChannelName:true,lazy:true,cardLayout:true,showDetailsMenu:true,defaultAction:'edit'});html+='</div>';html+='</div>';}
+html+=LibraryBrowser.getPosterViewHtml({items:group.items,shape:getSquareShape(),showTitle:true,showAirTime:true,showChannelName:true,lazy:true,cardLayout:true,showDetailsMenu:true,defaultAction:'edit'});html+='</div>';html+='</div>';}
 return Promise.resolve(html);}
 window.LiveTvHelpers={getDaysOfWeek:function(){var days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];return days.map(function(d){return{name:d,value:d};});},renderOriginalAirDate:function(elem,item){var airDate=item.PremiereDate;if(airDate&&item.IsRepeat){try{airDate=datetime.parseISO8601Date(airDate,true).toLocaleDateString();}
 catch(e){console.log("Error parsing date: "+airDate);}
