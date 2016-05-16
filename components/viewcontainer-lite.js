6789ed914d13752cce72d7f29de5c64b9d5ea034
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-16 15:51:22 -0400

    update components

--- components/viewcontainer-lite.js
+++ components/viewcontainer-lite.js
@@ -1,4 +1,4 @@
-﻿define(['browser'],function(browser){var allPages=document.querySelectorAll('.mainAnimatedPage');var currentUrls=[];var pageContainerCount=allPages.length;var animationDuration=500;var allowAnimation=true;var selectedPageIndex=-1;function enableAnimation(){if(!allowAnimation){return false;}
+﻿define(['browser'],function(browser){var allPages=document.querySelectorAll('.mainAnimatedPage');var currentUrls=[];var pageContainerCount=allPages.length;var allowAnimation=true;var selectedPageIndex=-1;function enableAnimation(){if(!allowAnimation){return false;}
 if(browser.tv){return false;}
 return true;}
 function loadView(options){if(options.cancel){return;}
@@ -10,7 +10,7 @@ var view;if(typeof(newView)=='string'){animatable.innerHTML=newView;view=animata
 enhanceNewView(dependencies,newView);view=newView;}
 if(onBeforeChange){onBeforeChange(view,false,options);}
 beforeAnimate(allPages,pageIndex,selected);animate(animatable,previousAnimatable,options.transition,options.isBack).then(function(){selectedPageIndex=pageIndex;currentUrls[pageIndex]=options.url;if(!options.cancel&&previousAnimatable){afterAnimate(allPages,pageIndex);}
-$.mobile=$.mobile||{};$.mobile.activePage=view;resolve(view);});});});}
+document.dispatchEvent(new CustomEvent('scroll',{}));$.mobile=$.mobile||{};$.mobile.activePage=view;resolve(view);});});});}
 function enhanceNewView(dependencies,newView){var hasJqm=false;for(var i=0,length=dependencies.length;i<length;i++){if(dependencies[i].indexOf('jqm')==0){hasJqm=true;break;}}
 if(hasJqm){$(newView).trigger('create');}}
 function replaceAll(str,find,replace){return str.split(find).join(replace);}
@@ -26,22 +26,19 @@ function nullAnimation(newAnimatedPage,oldAnimatedPage,transition,isBack){newAni
 function slide(newAnimatedPage,oldAnimatedPage,transition,isBack){var timings={duration:450,iterations:1,easing:'ease-out',fill:'both'}
 var animations=[];if(oldAnimatedPage){var destination=isBack?'100%':'-100%';animations.push(oldAnimatedPage.animate([{transform:'none',offset:0},{transform:'translate3d('+destination+', 0, 0)',offset:1}],timings));}
 newAnimatedPage.classList.remove('hide');var start=isBack?'-100%':'100%';animations.push(newAnimatedPage.animate([{transform:'translate3d('+start+', 0, 0)',offset:0},{transform:'none',offset:1}],timings));currentAnimations=animations;return new Promise(function(resolve,reject){animations[animations.length-1].onfinish=resolve;});}
-function fade(newAnimatedPage,oldAnimatedPage,transition,isBack){var timings={duration:animationDuration,iterations:1,easing:'ease-out',fill:'both'}
+function fade(newAnimatedPage,oldAnimatedPage,transition,isBack){var timings={duration:140,iterations:1,easing:'ease-out',fill:'both'}
 var animations=[];if(oldAnimatedPage){animations.push(oldAnimatedPage.animate([{opacity:1,offset:0},{opacity:0,offset:1}],timings));}
 newAnimatedPage.classList.remove('hide');animations.push(newAnimatedPage.animate([{opacity:0,offset:0},{opacity:1,offset:1}],timings));currentAnimations=animations;return new Promise(function(resolve,reject){animations[animations.length-1].onfinish=resolve;});}
 var currentAnimations=[];function cancelActiveAnimations(){var animations=currentAnimations;for(var i=0,length=animations.length;i<length;i++){cancelAnimation(animations[i]);}}
 function cancelAnimation(animation){try{animation.cancel();}catch(err){console.log('Error canceling animation: '+err);}}
 var onBeforeChange;function setOnBeforeChange(fn){onBeforeChange=fn;}
-function sendResolve(resolve,view){setTimeout(function(){resolve(view);},animationDuration);}
 function getSelectedIndex(allPages){return selectedPageIndex;}
 function tryRestoreView(options){var url=options.url;var index=currentUrls.indexOf(url);if(index!=-1){var page=allPages[index];var view=page.querySelector(".page-view");if(view){if(options.cancel){return;}
 cancelActiveAnimations();var animatable=allPages[index];var selected=getSelectedIndex(allPages);var previousAnimatable=selected==-1?null:allPages[selected];if(onBeforeChange){onBeforeChange(view,true,options);}
 beforeAnimate(allPages,index,selected);return animate(animatable,previousAnimatable,options.transition,options.isBack).then(function(){selectedPageIndex=index;if(!options.cancel&&previousAnimatable){afterAnimate(allPages,index);}
-$.mobile=$.mobile||{};$.mobile.activePage=view;return view;});}}
+document.dispatchEvent(new CustomEvent('scroll',{}));$.mobile=$.mobile||{};$.mobile.activePage=view;return view;});}}
 return Promise.reject();}
 function triggerDestroy(view){view.dispatchEvent(new CustomEvent("viewdestroy",{}));}
 function reset(){currentUrls=[];}
-function parentWithClass(elem,className){while(!elem.classList||!elem.classList.contains(className)){elem=elem.parentNode;if(!elem){return null;}}
-return elem;}
-function init(isAnimationAllowed){if(allowAnimation&&enableAnimation()&&!browser.animate){require(['webAnimations']);}}
-return{loadView:loadView,tryRestoreView:tryRestoreView,reset:reset,setOnBeforeChange:setOnBeforeChange,init:init};});
+if(enableAnimation()&&!browser.animate){require(['webAnimations']);}
+return{loadView:loadView,tryRestoreView:tryRestoreView,reset:reset,setOnBeforeChange:setOnBeforeChange};});
