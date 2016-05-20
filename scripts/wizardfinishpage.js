c1bfa142b778078320c2cb6093c765ea720025f4
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-05-20 17:44:56 -0400

    update components

--- scripts/wizardfinishpage.js
+++ scripts/wizardfinishpage.js
@@ -1,2 +1,2 @@
-﻿define(['jQuery'],function($){function onFinish(){ApiClient.ajax({url:ApiClient.getUrl('Startup/Complete'),type:'POST'}).then(function(){Dashboard.navigate('dashboard.html');});}
-$(document).on('pageinit','#wizardFinishPage',function(){$('.btnWizardNext',this).on('click',onFinish);});});
+﻿define(['loading'],function(loading){function onFinish(){loading.show();ApiClient.ajax({url:ApiClient.getUrl('Startup/Complete'),type:'POST'}).then(function(){Dashboard.navigate('dashboard.html');loading.hide();});}
+return function(view,params){var self=this;view.querySelector('.btnWizardNext').addEventListener('click',onFinish);};});
