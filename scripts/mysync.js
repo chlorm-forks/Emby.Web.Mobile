3803e5f9b44aed6f740fe4370e13f85834147eb9
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-07-02 22:30:39 -0400

    update components

--- scripts/mysync.js
+++ scripts/mysync.js
@@ -2,5 +2,5 @@
 if(status=="Active"){page.querySelector('.btnSyncNow').classList.add('hide');}
 else{page.querySelector('.btnSyncNow').classList.remove('hide');}}}
 function syncNow(page){LocalSync.sync();require(['toast'],function(toast){toast(Globalize.translate('MessageSyncStarted'));});refreshSyncStatus(page);}
-return function(view,params){var interval;view.querySelector('.btnSyncNow').addEventListener('click',function(){syncNow(view);});if(LocalSync.isSupported()){view.querySelector('.localSyncStatus').classList.remove('hide');}else{view.querySelector('.localSyncStatus').classList.add('hide');loading.hide();}
+return function(view,params){var interval;view.querySelector('.btnSyncNow').addEventListener('click',function(){syncNow(view);});if(LocalSync.isSupported()){view.querySelector('.localSyncStatus').classList.remove('hide');}else{view.querySelector('.localSyncStatus').classList.add('hide');}
 view.addEventListener('viewbeforeshow',function(){var page=this;refreshSyncStatus(page);interval=setInterval(function(){refreshSyncStatus(page);},5000);});view.addEventListener('viewbeforehide',function(){var page=this;loading.hide();if(interval){clearInterval(interval);interval=null;}});};});
