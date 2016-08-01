ba632c84f42c3e70918f8b8a9f8cefab6c8864bc
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-08-01 13:17:02 -0400

    update components

--- scripts/plugincatalogpage.js
+++ scripts/plugincatalogpage.js
@@ -1,4 +1,4 @@
-﻿define(['jQuery'],function($){var query={TargetSystems:'Server',IsAdult:false};function reloadList(page){Dashboard.showLoadingMsg();if(AppInfo.enableAppStorePolicy){$('.optionAdultContainer',page).hide();}else{$('.optionAdultContainer',page).show();}
+﻿define(['jQuery','cardStyle'],function($){var query={TargetSystems:'Server',IsAdult:false};function reloadList(page){Dashboard.showLoadingMsg();if(AppInfo.enableAppStorePolicy){$('.optionAdultContainer',page).hide();}else{$('.optionAdultContainer',page).show();}
 query.IsAppStoreSafe=true;var promise1=ApiClient.getAvailablePlugins(query);var promise2=ApiClient.getInstalledPlugins();Promise.all([promise1,promise2]).then(function(responses){populateList({catalogElement:$('#pluginTiles',page),noItemsElement:$("#noPlugins",page),availablePlugins:responses[0],installedPlugins:responses[1]});});}
 function populateList(options){populateListInternal(options);}
 function populateListInternal(options){var availablePlugins=options.availablePlugins;var installedPlugins=options.installedPlugins;var allPlugins=availablePlugins.filter(function(p){p.category=p.category||"General";p.categoryDisplayName=Globalize.translate('PluginCategory'+p.category.replace(' ',''));if(options.categories){if(options.categories.indexOf(p.category)==-1){return false;}}
@@ -11,15 +11,17 @@ return 0;});var html='';var i,length,plugin;var currentCategory;if(!options.cate
 if(b.installs>a.installs){return 1;}
 var aName=(a.name);var bName=(b.name);if(aName>bName){return 1;}
 if(bName>aName){return-1;}
-return 0;});var limit=screen.availWidth>=1920?15:12;for(i=0,length=Math.min(topPlugins.length,limit);i<length;i++){html+=getPluginHtml(topPlugins[i],options,installedPlugins);}}
-for(i=0,length=availablePlugins.length;i<length;i++){plugin=availablePlugins[i];var category=plugin.categoryDisplayName;if(category!=currentCategory){if(options.showCategory!==false){if(currentCategory){html+='<br/>';html+='<br/>';html+='<br/>';}
-html+='<div class="detailSectionHeader">'+category+'</div>';}
+return 0;});html+='<div class="itemsContainer vertical-wrap">';var limit=screen.availWidth>=1920?15:12;for(i=0,length=Math.min(topPlugins.length,limit);i<length;i++){html+=getPluginHtml(topPlugins[i],options,installedPlugins);}
+html+='</div>';}
+var hasOpenTag=false;for(i=0,length=availablePlugins.length;i<length;i++){plugin=availablePlugins[i];var category=plugin.categoryDisplayName;if(category!=currentCategory){if(options.showCategory!==false){if(currentCategory){hasOpenTag=false;html+='</div>';html+='<br/>';html+='<br/>';html+='<br/>';}
+html+='<div class="detailSectionHeader">'+category+'</div>';html+='<div class="itemsContainer vertical-wrap">';hasOpenTag=true;}
 currentCategory=category;}
 html+=getPluginHtml(plugin,options,installedPlugins);}
+if(hasOpenTag){html+='</div>';}
 if(!availablePlugins.length&&options.noItemsElement){$(options.noItemsElement).hide();}
 $(options.catalogElement).html(html);Dashboard.hideLoadingMsg();}
 function getPluginHtml(plugin,options,installedPlugins){var html='';var href=plugin.externalUrl?plugin.externalUrl:"addplugin.html?name="+encodeURIComponent(plugin.name)+"&guid="+plugin.guid;if(options.context){href+="&context="+options.context;}
-var target=plugin.externalUrl?' target="_blank"':'';html+="<div class='card backdropCard bottomPaddedCard'>";html+='<div class="cardBox visualCardBox">';html+='<div class="cardScalable">';html+='<div class="cardPadder"></div>';html+='<a class="cardContent" href="'+href+'"'+target+'>';if(plugin.thumbImage){html+='<div class="cardImage" style="background-image:url(\''+plugin.thumbImage+'\');">';}else{html+='<div class="cardImage" style="background-image:url(\'css/images/items/list/collection.png\');">';}
+var target=plugin.externalUrl?' target="_blank"':'';html+="<div class='card backdropCard bottomPaddedCard scalableCard'>";html+='<div class="cardBox visualCardBox">';html+='<div class="cardScalable">';html+='<div class="cardPadder"></div>';html+='<a class="cardContent" href="'+href+'"'+target+'>';if(plugin.thumbImage){html+='<div class="cardImage" style="background-image:url(\''+plugin.thumbImage+'\');">';}else{html+='<div class="cardImage" style="background-image:url(\'css/images/items/list/collection.png\');">';}
 if(plugin.isPremium){if(plugin.price>0){html+="<div class='premiumBanner'><img src='css/images/supporter/premiumflag.png' /></div>";}else{html+="<div class='premiumBanner'><img src='css/images/supporter/supporterflag.png' /></div>";}}
 html+="</div>";html+="</a>";html+="</div>";html+='<div class="cardFooter">';html+="<div class='cardText'>";html+=plugin.name;html+="</div>";if(!plugin.isExternal){html+="<div class='cardText' style='display:flex;align-items:center;'>";if(plugin.avgRating){html+='<i class="md-icon" style="color:#cc3333;margin-right:.25em;">star</i>';html+=plugin.avgRating.toFixed(1);}
 if(plugin.totalRatings){html+="<div style='margin-left:.5em;'>";html+=" "+Globalize.translate('LabelNumberReviews').replace("{0}",plugin.totalRatings);}
--- scripts/secondaryitems.js
+++ scripts/secondaryitems.js
@@ -1,4 +1,4 @@
-﻿define(['libraryBrowser','listView','emby-itemscontainer'],function(libraryBrowser,listView){return function(view,params){var data={};function addCurrentItemToQuery(query,item){if(item.Type=="Person"){query.PersonIds=item.Id;}
+﻿define(['libraryBrowser','listView','cardBuilder','emby-itemscontainer'],function(libraryBrowser,listView,cardBuilder){return function(view,params){var data={};function addCurrentItemToQuery(query,item){if(item.Type=="Person"){query.PersonIds=item.Id;}
 else if(item.Type=="Genre"){query.Genres=item.Name;}
 else if(item.Type=="MusicGenre"){query.Genres=item.Name;}
 else if(item.Type=="GameGenre"){query.Genres=item.Name;}
@@ -13,14 +13,15 @@ function getSavedQueryKey(){return libraryBrowser.getSavedQueryKey();}
 function parentWithClass(elem,className){while(!elem.classList||!elem.classList.contains(className)){elem=elem.parentNode;if(!elem){return null;}}
 return elem;}
 function onListItemClick(e){var mediaItem=parentWithClass(e.target,'mediaItem');if(mediaItem){var info=libraryBrowser.getListItemInfo(mediaItem);if(info.mediaType=='Photo'){var query=getQuery();require(['scripts/photos'],function(){Photos.startSlideshow(view,query,info.id);});return false;}}}
-function reloadItems(parentItem){Dashboard.showLoadingMsg();var query=getQuery(parentItem);ApiClient.getItems(Dashboard.getCurrentUserId(),query).then(function(result){window.scrollTo(0,0);var html='';var pagingHtml=libraryBrowser.getQueryPagingHtml({startIndex:query.StartIndex,limit:query.Limit,totalRecordCount:result.TotalRecordCount,showLimit:false});view.querySelector('.listTopPaging').innerHTML=pagingHtml;if(query.IncludeItemTypes=="Audio"){html='<div style="max-width:1000px;margin:auto;">'+listView.getListViewHtml({items:result.Items,playFromHere:true,action:'playallfromhere',smallIcon:true})+'</div>';}else{var posterOptions={items:result.Items,shape:"auto",centerText:true,lazy:true};if(query.IncludeItemTypes=="MusicAlbum"){posterOptions.overlayText=false;posterOptions.showParentTitle=true;posterOptions.showTitle=true;posterOptions.overlayPlayButton=true;}
+function onViewStyleChange(parentItem){var query=getQuery(parentItem);var itemsContainer=view.querySelector('#items');if(query.IncludeItemTypes=="Audio"){itemsContainer.classList.add('vertical-list');itemsContainer.classList.remove('vertical-wrap');}else{itemsContainer.classList.remove('vertical-list');itemsContainer.classList.add('vertical-wrap');}}
+function reloadItems(parentItem){Dashboard.showLoadingMsg();var query=getQuery(parentItem);ApiClient.getItems(Dashboard.getCurrentUserId(),query).then(function(result){window.scrollTo(0,0);var html='';var pagingHtml=libraryBrowser.getQueryPagingHtml({startIndex:query.StartIndex,limit:query.Limit,totalRecordCount:result.TotalRecordCount,showLimit:false});view.querySelector('.listTopPaging').innerHTML=pagingHtml;var itemsContainer=view.querySelector('#items');if(query.IncludeItemTypes=="Audio"){html=listView.getListViewHtml({items:result.Items,playFromHere:true,action:'playallfromhere',smallIcon:true});}else{var posterOptions={items:result.Items,shape:"auto",centerText:true,lazy:true};if(query.IncludeItemTypes=="MusicAlbum"){posterOptions.overlayText=false;posterOptions.showParentTitle=true;posterOptions.showTitle=true;posterOptions.overlayPlayButton=true;}
 else if(query.IncludeItemTypes=="MusicArtist"){posterOptions.overlayText=false;posterOptions.overlayPlayButton=true;}
 else if(query.IncludeItemTypes=="Episode"){posterOptions.overlayText=false;posterOptions.showParentTitle=true;posterOptions.showTitle=true;posterOptions.overlayPlayButton=true;}
-html=libraryBrowser.getPosterViewHtml(posterOptions);}
-var elem=view.querySelector('#items');elem.innerHTML=html+pagingHtml;ImageLoader.lazyChildren(elem);var i,length;var elems;function onNextPageClick(){query.StartIndex+=query.Limit;reloadItems(view);}
+html=cardBuilder.getCardsHtml(posterOptions);}
+itemsContainer.innerHTML=html+pagingHtml;ImageLoader.lazyChildren(itemsContainer);var i,length;var elems;function onNextPageClick(){query.StartIndex+=query.Limit;reloadItems(view);}
 function onPreviousPageClick(){query.StartIndex-=query.Limit;reloadItems(view);}
 elems=view.querySelectorAll('.btnNextPage');for(i=0,length=elems.length;i<length;i++){elems[i].addEventListener('click',onNextPageClick);}
 elems=view.querySelectorAll('.btnPreviousPage');for(i=0,length=elems.length;i<length;i++){elems[i].addEventListener('click',onPreviousPageClick);}
 Dashboard.hideLoadingMsg();});}
-view.addEventListener('click',onListItemClick);view.addEventListener('viewbeforeshow',function(e){if(params.parentId){ApiClient.getItem(Dashboard.getCurrentUserId(),params.parentId).then(function(parent){LibraryMenu.setTitle(parent.Name);reloadItems(parent);});}
-else{reloadItems();}});};});
+view.addEventListener('click',onListItemClick);view.addEventListener('viewbeforeshow',function(e){if(params.parentId){ApiClient.getItem(Dashboard.getCurrentUserId(),params.parentId).then(function(parent){LibraryMenu.setTitle(parent.Name);onViewStyleChange(parent);reloadItems(parent);});}
+else{onViewStyleChange();reloadItems();}});};});
