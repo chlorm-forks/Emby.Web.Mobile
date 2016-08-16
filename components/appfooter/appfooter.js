444118f6ff14db7849e69837cd33875b42158d0c
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-08-16 01:50:30 -0400

    update components

--- components/appfooter/appfooter.js
+++ components/appfooter/appfooter.js
@@ -1,5 +1,5 @@
 ﻿define(['css!./appfooter'],function(){function render(options){var elem=document.createElement('div');elem.classList.add('appfooter');document.body.appendChild(elem);return elem;}
-function initHeadRoom(instance,elem){require(["headroom"],function(){var headroom=new Headroom(elem,{tolerance:{down:20,up:0},classes:{pinned:'appfooter--pinned',unpinned:'appfooter--unpinned',top:'appfooter--top',notTop:'appfooter--not-top',initial:'appfooter-headroom'}});headroom.init();instance.headroom=headroom;});}
+function initHeadRoom(instance,elem){require(["headroom-window"],function(headroom){self.headroom=headroom;headroom.add(elem);});}
 function dockedTabs(options){var self=this;self.element=render(options);self.add=function(elem){self.element.appendChild(elem);};self.insert=function(elem){if(typeof elem==='string'){self.element.insertAdjacentHTML('afterbegin',elem);}else{self.element.insertBefore(elem,self.element.firstChild);}};initHeadRoom(self,self.element);}
-dockedTabs.prototype.destroy=function(){var self=this;if(self.headroom){self.headroom.destroy();}
-self.Element=null;};return dockedTabs;});
+dockedTabs.prototype.destroy=function(){var self=this;if(self.headroom){self.headroom.remove(self.element);self.headroom=null;}
+self.element=null;};return dockedTabs;});
