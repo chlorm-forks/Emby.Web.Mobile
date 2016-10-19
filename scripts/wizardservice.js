93c33a955b316719a2aae61267ee584f0ec3ed5b
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-10-19 14:30:23 -0400

    update components

--- scripts/wizardservice.js
+++
@@ -1,1 +0,0 @@
-define(["jQuery"],function($){$(document).on("pageinit","#wizardServicePage",function(){var page=this;$("#btnNextPage",page).on("click",function(){Dashboard.navigate("wizardagreement.html")})})});
